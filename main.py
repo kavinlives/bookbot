657c49e7155471b2e63ba44fc50a3ef09c97c093
kavinlives <kavin.sathish4233@gmail.com>
2025-12-19 08:46:23 +0000

    a description of your changes here

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys
 from stats import *
 
 def main():
+    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
     print("============ BOOKBOT ============")
     
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     print(f"Analyzing book found at {book_path}...")
     
     print("----------- Word Count ----------")
